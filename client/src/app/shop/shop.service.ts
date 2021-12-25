e8470188984fd9f437ee966845f51f4b73b1dfdb
Amjath <aamjathmohd@gmail.com>
2021-12-25 12:54:54 +0530

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map} from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -44,5 +45,9 @@ export class ShopService {
 
   getTypes(){
     return this.http.get<IType[]>(this.baseUrl+'products/types');
+  }
+
+  getProduct(id:number){
+    return this.http.get<IProduct>(this.baseUrl+'products/'+id);
   }
 }
